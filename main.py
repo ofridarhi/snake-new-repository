b64f1d44fb43a5dc23d7e1c3998522addb2c4082
User <ofridarhi12345@gmail.com>
2022-08-11 15:26:36 +0300

    started over the whole code, fixed the create board func

--- main.py
+++ main.py
@@ -6,38 +6,32 @@ WHITE = (255,255,255)
 FPS = 60
 
 WIDTH, HEIGHT = 800,800
+SQUARE_SIZE = WIDTH // 10
+ROWS = WIDTH // SQUARE_SIZE
+COLS = HEIGHT // SQUARE_SIZE
+
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("snake")
 
+
 class Board():
-    def __init__(self,internal_board):
-        self.internal_board = internal_board
+    def create_board(self):
+        WIN.fill(WHITE)
+        for row in range(ROWS):
+            for col in range(COLS):
+                pygame.draw.rect(WIN,BLACK,(row *SQUARE_SIZE,col * SQUARE_SIZE, SQUARE_SIZE,SQUARE_SIZE),2)
 
-    def create_internal_board(self):
-        self.internal_board = numpy.zeros((10,10))
-        print(self.internal_board)
 
-    def create_visual_board(self):
-        for i in range(self.internal_board):
-            for j in range(self.internal_board[i]):
-                pygame.draw.rect(WIN,BLACK,(HEIGHT /10 * i,HEIGHT /10 * i))
-
-class Snake(pygame.sprite.Sprite):
-    def __init__(self,width,height,pos_x,pos_y,color):
-        super().__init__()
 
 def main():
 
     board = Board()
     run = True
     clock = pygame.time.Clock()
+    board.create_board()
     while run:
         clock.tick(FPS)
-        WIN.fill(WHITE)
-        board.create_internal_board()
-        board.create_visual_board()
         pygame.display.update()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
