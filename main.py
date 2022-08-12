18b4917e5976b2a90045f63b4ac896af7122adac
User <ofridarhi12345@gmail.com>
2022-08-12 18:57:05 +0300

    Create moving snake

--- main.py
+++ main.py
@@ -2,30 +2,40 @@ import pygame
 import numpy
 import time
 
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-GREEN = (0,255,0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
 FPS = 120
 
-WIDTH, HEIGHT = 800,800
+WIDTH, HEIGHT = 800, 800
 SQUARE_SIZE = WIDTH // 10
 ROWS = WIDTH // SQUARE_SIZE
 COLS = HEIGHT // SQUARE_SIZE
 PADDING = 2
 
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("snake")
 
+DIRECTION_DICT = dict()
+DIRECTION_DICT['right'] = (1, 0)
+DIRECTION_DICT['left'] = (-1, 0)
+DIRECTION_DICT['up'] = (0, -1)
+DIRECTION_DICT['down'] = (0, 1)
 
-class Board():
+
+class Board:
+    def __init__(self):
+        self.create_board()
+
     def create_board(self):
         WIN.fill(WHITE)
         for row in range(ROWS):
             for col in range(COLS):
-                pygame.draw.rect(WIN,BLACK,(row *SQUARE_SIZE,col * SQUARE_SIZE, SQUARE_SIZE,SQUARE_SIZE),PADDING)
+                pygame.draw.rect(WIN, BLACK, (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), PADDING)
+
 
 class Snake:
-    def __init__(self,x,y,color,width,height):
+    def __init__(self, x, y, color, width, height):
         self.x = x
         self.y = y
         self.x_vel = SQUARE_SIZE
@@ -33,47 +43,62 @@ class Snake:
         self.color = color
         self.width = width
         self.height = height
-        self.squars = []
+        self.direction = DIRECTION_DICT['right']
+        self.snake_cells = [(self.x, self.y)]
 
-    def create_snake_head(self):
-        pygame.draw.rect(WIN,self.color,(self.x,self.y,self.width,self.height))
+    # def eat(self):
+    #     keys = pygame.key.get_pressed()
+    #     if keys[pygame.K_a]:
+    #         last_cell = self.snake_parts[-1]
+    def update_snake(self):
+        snake_cells = []
+        direction_x, direction_y = self.direction
+        for cell_x, cell_y in self.snake_cells:
+            new_cell = (cell_x + direction_x * self.x_vel, cell_y + direction_y * self.y_vel)
+            snake_cells.append(new_cell)
+        self.snake_cells = snake_cells
+
+    def draw(self):
+        self.update_snake()
+        for cell_x, cell_y in self.snake_cells:
+            pygame.draw.rect(WIN, self.color, (cell_x, cell_y, self.width, self.height))
         pygame.display.update()
 
-    def move_snake_head(self):
-        time.sleep(0.3)
+    def change_snake_direction(self):
+        time.sleep(0.1)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
-            pygame.draw.rect(WIN, WHITE, (self.x, self.y, self.width, self.height))
-            self.y -= self.y_vel
+            self.direction = DIRECTION_DICT['up']
         elif keys[pygame.K_DOWN]:
-            pygame.draw.rect(WIN, WHITE, (self.x, self.y, self.width, self.height))
-            self.y += self.y_vel
+            self.direction = DIRECTION_DICT['down']
         elif keys[pygame.K_LEFT]:
-            pygame.draw.rect(WIN, WHITE, (self.x, self.y, self.width, self.height))
-            self.x -= self.x_vel
+            self.direction = DIRECTION_DICT['left']
         elif keys[pygame.K_RIGHT]:
-            pygame.draw.rect(WIN, WHITE, (self.x, self.y, self.width, self.height))
-            self.x += self.x_vel
-        pygame.draw.rect(WIN, self.color, (self.x, self.y, self.width, self.height))
-        pygame.display.update()
+            self.direction = DIRECTION_DICT['right']
+
+    def get_head_x_y(self):
+        x = self.x
+        y = self.y
+        return x, y
 
 
 def main():
-    snake = Snake(SQUARE_SIZE * 5 +PADDING, SQUARE_SIZE * 5 +PADDING,GREEN,SQUARE_SIZE -PADDING*2,SQUARE_SIZE -PADDING*2)
+    snake = Snake(SQUARE_SIZE * 5 + PADDING, SQUARE_SIZE * 5 + PADDING, GREEN, SQUARE_SIZE - PADDING * 2,
+                  SQUARE_SIZE - PADDING * 2)
     board = Board()
     run = True
     clock = pygame.time.Clock()
-    board.create_board()
+
     while run:
         clock.tick(FPS)
-        snake.create_snake_head()
-        snake.move_snake_head()
-        pygame.display.update()
+        snake.change_snake_direction()
+        snake.draw()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-            #if event.type == pygame.KEYUP
+            # if event.type == pygame.KEYUP
+
 
 if __name__ == '__main__':
     main()
